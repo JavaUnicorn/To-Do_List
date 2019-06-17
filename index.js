1a4a6cf9977bb85b0b648c30ad273a460c93a840
Apple <apple@Apples-MacBook-Pro.local>
2019-06-17 12:00:30 -0600

    trying to add a delete button

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var cardContainer = $('.card-container');
 var submit = $('.submit-btn');
 
+
+// delete.on("click", deleteTask);
+// function deleteTask() {
+// $('.cardContainer').val("");
+// }
+$("button").click(function)(){
+  $("section").remove(":contains('card-container')");});
+}
+
+  // editButton.onclick=editTask;
+  // 			//Bind deleteTask to delete button.
+  // 			deleteButton.onclick=deleteTask;
+
+
+
 submit.on("click", appendSomeoneAwesome);
 function appendSomeoneAwesome() {
   var name = $('.name').val();
 
   cardContainer.append(`
-    <p class="name-card">${name} is awesome!</p>
+    <p class="name-card">${name}</p>
   `);
-
+localStorage.setItem("Event", `.name`);
    $('.name').val("");
 
 }
